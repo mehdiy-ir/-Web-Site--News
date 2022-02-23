400e84cae1254b48c79ef2f626a455f5aa5aa31c
Alirg <ali.programer.1999@gmail.com>
2022-02-23 20:43:45 +0330

    middel left

--- js/app.js
+++ js/app.js
@@ -14,3 +14,21 @@ let changer = 0;
         slides.style.right = 0
     }
  },4000)
+
+// ================================================
+const leftslide = document.querySelector(".middel-left");
+const leftsid = leftslide.childElementCount;
+const leftrun = (leftsid - 1) * 100 * -1;
+
+let runner = 0;
+ setInterval (function(){
+     if(runner>leftrun){
+         runner -= 100;
+         leftslide.style.top = runner + '%';
+     }
+     else{
+         runner = 0;
+         leftslide.style.top = 0;
+
+     }
+ },4000)
